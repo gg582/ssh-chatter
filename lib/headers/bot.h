c23ebcde3b1a23d720472277be5216f3b6c49e64
hobbycoder <168180007+gg582@users.noreply.github.com>
2025-10-15 20:53:42 +0900

    Add captcha hints and interactive BBS composer

--- lib/headers/bot.h
+++ lib/headers/bot.h
@@ -2,6 +2,7 @@
 #define SSH_CHATTER_BOT_H
 
 #include <stdbool.h>
+#include <time.h>
 
 struct host;
 struct client_manager;
@@ -13,5 +14,8 @@ bool chat_bot_start(chat_bot_t *bot);
 void chat_bot_shutdown(chat_bot_t *bot);
 void chat_bot_destroy(chat_bot_t *bot);
 bool chat_bot_is_enabled(const chat_bot_t *bot);
+void chat_bot_set_captcha_hint(chat_bot_t *bot, const char *question, const char *answer);
+bool chat_bot_snapshot_captcha_hint(chat_bot_t *bot, char *question, size_t question_length, char *answer,
+                                    size_t answer_length, struct timespec *timestamp);
 
 #endif
