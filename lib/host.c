be03cc4fdb0c6a86b483cb8cd5ace97d0e22349f
gg582 <gzblues61@daum.net>
2025-10-15 16:12:24 +0900

    modify megami theme

--- lib/host.c
+++ lib/host.c
@@ -90,7 +90,7 @@ typedef struct palette_descriptor {
 static const palette_descriptor_t PALETTE_DEFINITIONS[] = {
     {"moe", "Soft magenta accents with playful highlights", "bright-magenta", "white", true, "white", "bright-magenta", "cyan", true},
     {"neon-genesis-evangelion", "Sho-nen yo Shin-wa ni nare--", "bright-magenta", "white", true, "white", "bright-red", "magenta", true},
-    {"megami", "Japanese anime goddess cliche", "bright-white", "black", true, "black", "bright-white", "black", true},
+    {"megami", "Japanese anime goddess cliché", "bright-white", "black", false, "black", "bright-white", "black", false},
     {"clean", "Balanced neutral palette", "default", "default", false, "default", "default", "default", false},
     {"adwaita", "Bright background inspired by GNOME Adwaita", "blue", "default", false, "blue", "bright-white", "white", true},
     {"80shacker", "Bright monochrome green inspired by old CRT", "bright-green", "default", true, "bright-green", "default", "default", true},
