019c9322e42f9dff3e9dbba608e8f7c7f0d2ed7a
Lee Yunjin <gzblues61@daum.net>
2025-10-15 11:35:03 +0900

    motd does not show up well

--- lib/host.c
+++ lib/host.c
@@ -4971,13 +4971,14 @@ static bool host_try_load_motd_from_path(host_t *host, const char *path) {
   session_normalize_newlines(motd_buffer);
 
   pthread_mutex_lock(&host->lock);
-//  snprintf(host->motd, sizeof(host->motd), "%s", motd_buffer);
   char motd_clean[4096];
-  char * motd_line = strtok(host->motd, "\n");
+  char *next_line;
+  char * motd_line = strtok_r(host->motd, "\n", &next_line);
   while(motd_line != NULL) {
-    snprintf(motd_clean, strlen(motd_line), "\033[lG%s\n", motd_line);
+    snprintf(motd_clean, strlen(motd_line), "\033[1G%s\n", motd_line);
+    motd_line = strtok_r(NULL, "\n", &next_line);
   }
-  strcpy(host->motd, motd_clean);
+  snprintf(host->motd, strlen(motd_clean), "%s", motd_clean);
   pthread_mutex_unlock(&host->lock);
   return true;
 }
